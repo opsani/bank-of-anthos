c317eecd105632c10e1abbd4f04816c8c417b441
Robert Starmer <rstarmer@gmail.com>
2021-08-25 14:50:39 -0700

    Update to support custom metrics from prometheus into HPA autoscaling/v2beta2

--- helm/setup_grafana_prometheus.sh
+++ helm/setup_grafana_prometheus.sh
@@ -5,14 +5,14 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo update
 
-echo create the monitoring namespace and install prometheus and grafana
-kubectl create ns monitoring
-helm install kube-prometheus-stack prometheus-community/prometheus --namespace monitoring
-helm install grafana grafana/grafana --namespace monitoring
+echo create the metrics namespace and install prometheus and grafana
+kubectl create ns metrics
+helm install prometheus prometheus-community/prometheus --namespace metrics
+helm install prom-adapter prometheus-community/prometheus-adapter --namespace metrics --values prom-adapter-values.yaml
+helm install grafana grafana/grafana --namespace metrics --values grafana-values.yaml
 
-echo "Add additional metrics monitoring for prometheus"
-kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+#echo "Add additional metrics for prometheus"
+#kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 
 echo "check metrics availability with: kubectl get --raw /apis/custom.metrics.k8s.io/v1beta1"
-helm install kube-prometheus-alerts prometheus-community/prometheus-adapter --namespace monitoring
 
