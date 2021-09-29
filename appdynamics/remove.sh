18b1c5290dff0d6c7e00f66871d4e7752c669533
Daniel Howell <self@danielhhowell.com>
2021-09-28 22:35:13 -0500

    Add default NS to removal

--- appdynamics/remove.sh
+++ appdynamics/remove.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Removing bank of anthos"
-kubectl -n ${NAMESPACE} delete -f ../kubernetes-manifests
+kubectl -n ${NAMESPACE:-bank-of-anthos-appdynamics} delete -f ../kubernetes-manifests
 kubectl delete -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
 kubectl delete ns ${NAMESPACE:-bank-of-anthos-appdynamics}
